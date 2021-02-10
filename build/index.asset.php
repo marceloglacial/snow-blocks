4ec21fdc199a88c8c2d2fc5aebc0ece7072a5bc2
Marcelo <glacial@gmail.com>
2021-02-09 21:07:18 -0500

    reset styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ae1511831d1108c30d4e6ae6e8658bc4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'c2c5ed878f80eb95443a0f1b7e43fe77');
